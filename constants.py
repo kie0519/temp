8c28e719947cc263958b1cc39e0faedfd19e9ac2
左俊威 <junwei.zuo@aminer.cn>
2025-11-17 19:37:33 +0800

    feat(constants): 添加常量信息查询函数

--- constants.py
+++ constants.py
@@ -1,5 +1,6 @@
 """
 数学常量模块
+提供常用的数学常量和转换因子
 """
 
 import math
@@ -14,3 +15,14 @@ GOLDEN_RATIO = (1 + math.sqrt(5)) / 2
 # 常用角度（弧度）
 DEG_TO_RAD = PI / 180
 RAD_TO_DEG = 180 / PI
+
+
+def get_constant_info():
+    """获取所有常量信息"""
+    return {
+        "PI": PI,
+        "E": E,
+        "黄金比例": GOLDEN_RATIO,
+        "角度转弧度": DEG_TO_RAD,
+        "弧度转角度": RAD_TO_DEG
+    }
